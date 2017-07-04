55649e4f68fcd91a94c603b85e899e07b97df4dc
Roman Malkevych <henprokuror@gmail.com>
2017-07-04 11:27:54 +0300

    GetNextLine

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmalkevy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/26 16:47:50 by rmalkevy          #+#    #+#             */
+/*   Updated: 2016/12/29 18:58:24 by rmalkevy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __GET_NEXT_LINE_H
+# define __GET_NEXT_LINE_H
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# define BUFF_SIZE 4
+# include "libft/libft.h"
+
+int get_next_line(const int fd, char **line);
+#endif
